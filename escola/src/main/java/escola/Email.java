af861420eb421e6e61456223f85394b6f61995f7
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-02-23 22:35:16 -0300

    criando a classe email "Value Objects"

--- escola/src/main/java/escola/Email.java
+++ escola/src/main/java/escola/Email.java
@@ -1,4 +1,13 @@
 package escola;
 
 public class Email {
+
+    private String endereco;
+
+    public Email(String endereco) {
+        if (endereco == null || !endereco.matches("/^[a-z0-9.]+@[a-z0-9]+\\.[a-z]+\\.([a-z]+)?$/i")) {
+            throw new IllegalArgumentException("E-mail inválido");
+        }
+        this.endereco = endereco;
+    }
 }

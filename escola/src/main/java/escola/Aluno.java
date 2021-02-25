544ae0ab2334b8b193cb3b2f5f5e18ee1dd42590
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-02-25 18:33:52 -0300

    add classe de indicações

--- escola/src/main/java/escola/Aluno.java
+++ escola/src/main/java/escola/Aluno.java
@@ -10,7 +10,25 @@ public class Aluno {
     private Email email;
     private List<Telefone> telefones = new ArrayList<>();
 
+    public Aluno(CPF cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
     public void adicionarTelefone(String ddd, String numero) {
         this.telefones.add(new Telefone(ddd, numero));
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Email getEmail() {
+        return email;
     }
 }
